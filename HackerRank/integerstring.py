e5a060ccfe71ddf2e972eb61586ec1af001f4cc2
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-04-12 04:26:47 +0530

    Create integerstring.py

---
+++ HackerRank/integerstring.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    for x in range (1,n+1):
+        print(x,end="")
+        
+        
+        
+"""
+Print the list of integers from  through  as a string, without spaces.
+Sample Input
+3
+Sample Output 0
+123
+"""
