1b642cd30ef0bb90720a634b80092aad0f7077c6
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-04-24 05:12:13 +0530

    Create Power_ModPower.py

---
+++ HackerRank/Power_ModPower.py
@@ -0,0 +1,9 @@
+#problem - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+a=int(input())
+b=int(input())
+m=int(input())
+x=a**b
+y= x %m
+print(x)
+print(y)
