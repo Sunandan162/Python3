fc10f16a9dae2451f76084b2435069c82138f110
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-04-24 05:17:48 +0530

    Create IntegersComesInAllSize.py

---
+++ HackerRank/IntegersComesInAllSize.py
@@ -0,0 +1,7 @@
+#problem- https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print((a**b)+(c**d))
