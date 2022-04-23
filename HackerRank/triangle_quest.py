7e02c04f19dab1064d67c8f7ff89f2513035102e
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-04-24 05:22:59 +0530

    Create triangle_quest.py

---
+++ HackerRank/triangle_quest.py
@@ -0,0 +1,5 @@
+#prob- https://www.hackerrank.com/challenges/python-quest-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+for i in range(1,int(input())): 
+    print((10**(i)//9)*i)
