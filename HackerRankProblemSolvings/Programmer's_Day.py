1243e01e525b48a0cd37cedcaa672e745321d394
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-04-28 05:04:45 +0530

    Update Programmer's_Day.py

--- HackerRankProblemSolvings/Programmer's_Day.py
+++ HackerRankProblemSolvings/Programmer's_Day.py
@@ -1,24 +1,23 @@
 #problem - https://www.hackerrank.com/challenges/day-of-the-programmer/problem?isFullScreen=true
 
 y=int(input())
-leap=False
 if y==1918:
     print("26.09.",end="")
     print(y)
-elif y>1918:
-    if y % 400 == 0 :
+else:
+    if y>1918:
+        if y % 400 == 0 :
+                leap = True
+        elif y %100 != 0 and y % 4 == 0 :
             leap = True
-    elif y %100 != 0 and y % 4 == 0 :
-        leap = True
-    else :
-        leap = False  
-elif y<=1917:
-    leap=False
-    if y % 4 == 0 :
-        leap = True
-    else :
-        leap = False   
-if y != 1918:
+        else :
+            leap = False  
+    elif y<=1917:
+        leap=False
+        if y % 4 == 0 :
+            leap = True
+        else :
+            leap = False   
     if leap==True:
         print("12.09.",end="")
     else:
