bb8c9b96c6de5ccab264b8d84d7027e8c9d98f17
Sunandan162 <103312829+Sunandan162@users.noreply.github.com>
2022-05-09 15:52:55 +0530

    Create Drawing_book.py

---
+++ HackerRankProblemSolvings/Drawing_book.py
@@ -0,0 +1,6 @@
+#problem - https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+n = int(input().strip())
+p = int(input().strip())
+a = min(p//2,n//2-p//2)
+print(int(a))
